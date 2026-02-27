9cd7a23efb1609341b341657b41d741d8eadb163
konohajogos <konohajogosroo@gmail.com>
2026-02-27 00:07:45 -0300

    add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Space_Grotesk, Inter } from 'next/font/google'
-
+import Script from "next/script";
 import './globals.css'
 
 const _spaceGrotesk = Space_Grotesk({
@@ -33,6 +33,23 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${_spaceGrotesk.variable} ${_inter.variable} font-sans antialiased`}>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1728076811102929');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         {children}
       </body>
     </html>
